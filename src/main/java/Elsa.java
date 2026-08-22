2a83d37d2d190659bc0046d5c27bb5a32d57b85a
Koh Yu Jian <yujian2k04@gmail.com>
2026-08-22 14:43:02 +0800

    Greet the user and exit

--- src/main/java/Elsa.java
+++ src/main/java/Elsa.java
@@ -3,12 +3,22 @@
  */
 public class Elsa {
     public static void main(String[] args) {
+        String border = "____________________________________________________________";
         // Each "\\" in the source produces a single backslash in the ASCII-art banner.
         String banner = " _____ _           \n"
                 + "|  ___| |___  __ _ \n"
                 + "| |__ | / __|/ _` |\n"
                 + "|  __|| \\__ \\ (_| |\n"
-                + "|_____|_|___/\\__,_|\n";
+                + "|_____|_|___/\\__,_|";
+        String greeting = "Hello! I'm Elsa.\n"
+                + "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(border);
         System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(border);
+        System.out.println(farewell);
+        System.out.println(border);
     }
 }
