5f6a46e1621e9df7862c48956f8cc8e362eb3890
Koh Yu Jian <yujian2k04@gmail.com>
2026-08-22 14:27:12 +0800

    Rename chatbot from Duke to Elsa

---
+++ src/main/java/Elsa.java
@@ -0,0 +1,14 @@
+/**
+ * Entry point of the Elsa chatbot.
+ */
+public class Elsa {
+    public static void main(String[] args) {
+        // Each "\\" in the source produces a single backslash in the ASCII-art banner.
+        String banner = " _____ _           \n"
+                + "|  ___| |___  __ _ \n"
+                + "| |__ | / __|/ _` |\n"
+                + "|  __|| \\__ \\ (_| |\n"
+                + "|_____|_|___/\\__,_|\n";
+        System.out.println(banner);
+    }
+}
